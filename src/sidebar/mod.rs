140851bddc110880557f01d2af76d357d1eb1cbe
DasLixou <einlixou@gmail.com>
2023-02-14 17:49:56 +0100

    Selecting Items ^^

--- src/sidebar/mod.rs
+++ src/sidebar/mod.rs
@@ -6,37 +6,49 @@ use self::element::SidebarButton;
 
 pub const SIDEBAR_ELEMENT_SIZE: Units = Pixels(60.);
 
+pub enum SidebarEvent {
+    SelectItem(usize),
+}
+
 #[derive(Lens, Clone)]
-pub struct SidebarElement {
+pub struct SidebarItem {
     pub text: String,
 }
 
 #[derive(Lens)]
 pub struct Sidebar {
-    pub sidebar_elements: Vec<SidebarElement>,
+    pub sidebar_items: Vec<SidebarItem>,
     pub selected: usize,
 }
 
-impl Model for Sidebar {}
+impl Model for Sidebar {
+    fn event(&mut self, _cx: &mut EventContext, event: &mut Event) {
+        event.map(|sidebar_event, _| match sidebar_event {
+            SidebarEvent::SelectItem(index) => {
+                self.selected = *index;
+            }
+        })
+    }
+}
 
 pub fn create_sidebar<'a>(cx: &'a mut Context) -> Handle<impl View> {
     cx.add_stylesheet("src/sidebar/style.css").unwrap();
 
     Sidebar {
-        sidebar_elements: vec![
-            SidebarElement {
+        sidebar_items: vec![
+            SidebarItem {
                 text: "Akari".into(),
             },
-            SidebarElement {
+            SidebarItem {
                 text: "Scribe".into(),
             },
-            SidebarElement {
+            SidebarItem {
                 text: "Books".into(),
             },
-            SidebarElement {
+            SidebarItem {
                 text: "Calendar".into(),
             },
-            SidebarElement {
+            SidebarItem {
                 text: "Settings".into(),
             },
         ],
@@ -44,11 +56,11 @@ pub fn create_sidebar<'a>(cx: &'a mut Context) -> Handle<impl View> {
     }
     .build(cx);
 
-    List::new(cx, Sidebar::sidebar_elements, |cx, index, item| {
+    List::new(cx, Sidebar::sidebar_items, |cx, index, item| {
         SidebarButton::new(
             cx,
-            item.then(SidebarElement::text),
-            |_| {},
+            item.then(SidebarItem::text),
+            move |cx| cx.emit(SidebarEvent::SelectItem(index)),
             Sidebar::selected.map(move |i| index == *i),
         );
     })
